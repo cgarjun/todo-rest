bbf8ce9686e616c5725f3d796e1a58f5d938495c
Arjun Thekkumadathil <arjun@Arjuns-MacBook-Pro.local>
2018-01-06 15:25:01 +1100

    todo api completed

--- app.py
+++ app.py
@@ -142,5 +142,72 @@ def login():
 
 	return make_response("Could not verify the login credentials", 401, {'WWW-Authenticate':'Baic realm = "Login required"'})
 
+@app.route('/todo', methods=['GET'])
+@token_required
+def get_all_todods(current_user):
+	todos = Todo.query.filter_by(user_id=current_user.id).all()
+	output = []
+
+	for todo in todos:
+		todo_data = {}
+		todo_data['id'] = todo.id
+		todo_data['text'] = todo.text
+		todo_data['complete'] = todo.complete
+		output.append(todo_data)
+
+	return jsonify({'todos':output})
+
+@app.route('/todo/<todo_id>', methods=['GET'])
+@token_required
+def get_one_todo(current_user, todo_id):
+	todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+
+	if not todo:
+		return jsonify({'message':'No todo found'})
+
+	todo_data = {}
+	todo_data['id'] = todo.id
+	todo_data['text'] = todo.text
+	todo_data['complete'] = todo.complete
+
+	return jsonify(todo_data)
+
+@app.route('/todo', methods=['POST'])
+@token_required
+def create_todo(current_user):
+	data = request.get_json()
+
+	new_todo = Todo(text=data['text'], complete=False, user_id=current_user.id)
+	db.session.add(new_todo)
+	db.session.commit()
+
+	return jsonify({'message':'New todo item created'})
+
+@app.route('/todo/<todo_id>', methods=['PUT'])
+@token_required
+def complete_todo(current_user, todo_id):
+	todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+
+	if not todo:
+		return jsonify({'message':'No todo found'})
+
+	todo.complete = True
+	db.session.commit()
+	return jsonify({'message':'Todo item completed.'})
+
+@app.route('/todo/<todo_id>', methods=['DELETE'])
+@token_required
+def delete_todo(current_user, todo_id):
+	todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+
+	if not todo:
+		return jsonify({'message':'No todo found'})
+
+	db.session.delete(todo)
+	db.session.commit()
+	return jsonify({'message':'The todo has been deleted'})
+
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
